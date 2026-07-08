d5a25efc76594cfeec90f9ba25e24c98de64955f
Vertina_174 <personvip33@gmail.com>
2026-07-08 16:38:39 +0530

    Improvised the Overall performance of the game...

--- hi1.cpp
+++ hi1.cpp
@@ -11,25 +11,10 @@ int board[3][3] ={
     {0,0,0},
     {0,0,0}
 };
-int player=0,counter=0,winner=0;
-void drawboard(){
-DrawLine(300, 100, 300, height*0.85, WHITE);
-DrawLine(500, 100, 500, height*0.85, WHITE);
+int player=0;
+int counter=0;
+int winner=0;
 
-DrawLine(180, 215, width*0.79, 215, WHITE);
-DrawLine(180, 385, width*0.79, 385, WHITE);
-
-DrawText("Player: ",5,5,50,WHITE);
-
-if(!player){
-    DrawRectangle(190,5,50,70,BLACK);
-    DrawText("o",190,-20,100,WHITE);
-}
-else{
-    DrawRectangle(190,5,50,70,BLACK);
-    DrawText("x",190,-20,100,WHITE);
-}
-}
 int checkwinner(){
     for(int i=0; i<3; i++){// checking for rows
         if(board[i][0]!=0){
@@ -57,130 +42,55 @@ int checkwinner(){
     }
     return 0;
 }
+void HandleInput(){
+    if(!IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+        return;
 
-void playermove(int position){
-    ++counter;
-    switch(position){
-        case 1:{
-            if(player==0){
-                DrawText("o",200,70,150,BLUE);
-                board[0][0]=1;
-                player=1;                
-            }
-            else{
-                DrawText("x",200,70,150,RED);
-                board[0][0]=2;
-            player=0;
-                }
-            break;
-        }
-        case 2:{
-            if(player==0){
-                DrawText("o",350,70,150,BLUE);
-                board[0][1]=1;
-                player=1;                
-            }
-            else{
-                DrawText("x",350,70,150,RED);
-                board[0][1]=2;
-                player=0;
-                }
-                break;
-        }
-        case 3:{
-             if(player==0){
-                DrawText("o",521,70,150,BLUE);
-                board[0][2]=1;
-                player=1;                
-            }
-            else{
-                DrawText("x",521,70,150,RED);
-                board[0][2]=2;
-                player=0;
-                }
-            break;
-        }
-        case 4:{
-            if(player==0){
-                DrawText("o",200,216,150,BLUE);
-                board[1][0]=1;
-                player=1;                
-            }
-            else{
-                DrawText("x",200,216,150,RED);
-                board[1][0]=2;
-                player=0;
-                }
-            break;
-        }
-        case 5:{
-            if(player==0){
-                DrawText("o",350,216,150,BLUE);
-                board[1][1]=1;
-                player=1;                
-            }
-            else{
-                DrawText("x",350,216,150,RED);
-                board[1][1]=2;
-                player=0;
-                }
-            break;
-        }
-        case 6:{
-            if(player==0){
-                DrawText("o",521,216,150,BLUE);
-                board[1][2]=1;
-                player=1;                
-            }
-            else{
-                DrawText("x",521,216,150,RED);
-                board[1][2]=2;
-                player=0;
-                }
-            break;
-        }
-        case 7:{
-            if(player==0){
-                DrawText("o",200,370,150,BLUE);
-                board[2][0]=1;
-                player=1;                
-            }
-            else{
-                DrawText("x",200,370,150,RED);
-                board[2][0]=2;
-                player=0;
-                }
-            break;
-        }
-        case 8:{
-            if(player==0){
-                DrawText("o",350,370,150,BLUE);
-                board[2][1]=1;
-                player=1;                
-            }
-            else{
-                DrawText("x",350,370,150,RED);
-                board[2][1]=2;
-                player=0;
-                }
-            break;
-        }
-        case 9:{
-            if(player==0){
-                DrawText("o",521,360,150,BLUE);
-                board[2][2]=1;
-                player=1;                
-            }
-            else{
-                DrawText("x",521,360,150,RED);
-                board[2][2]=2;
-                player=0;
-                }
-            break;
-        }
+    int mouseX=GetMouseX();
+    int mouseY= GetMouseY();
+    if(mouseX<180 && mouseX>621)
+        return;
+    if(mouseY<100 && mouseY>501)
+        return;
+        
+        int y = (mouseX-180)/140;
+        int x = (mouseY-100)/133;
+
+    if(board[x][y]!=0)
+        return;
+ 
+            board[x][y]= player+1;
+            player = !player;
+            ++counter;
+winner = checkwinner();            
+     
+}
+
+void drawboard(){
+    DrawLine(300,100,300,510,WHITE);
+    DrawLine(480,100,480,510,WHITE);
+
+    DrawLine(180,215,620,215,WHITE);
+    DrawLine(180,385,620,385,WHITE);
+}
+void drawmarks(){
+    int startx = 200;
+    int starty = 70;
+    int cellwidth = 150;
+    int cellheight = 145;
+for(int i=0; i<3; i++){
+    for(int j=0; j<3; j++){
+        int x = startx + j*cellwidth;
+        int y = starty + i*cellheight;
+        if(board[i][j]==1)
+            DrawText("o",x,y,150,BLUE);
+        else if(board[i][j]==2)
+            DrawText("x",x,y,150,RED);
+        
     }
-
-    winner = checkwinner();
+}
+}
+void drawwinner(){
     if(winner == 1){
         DrawText("Player 1 Wins!!!",110,height-90,80,YELLOW);
     }
@@ -191,118 +101,56 @@ void playermove(int position){
         DrawText("DRAW",300,height-90,80,YELLOW);
     }
 }
-void getmouseposition(){
-    int x=GetMouseX();
-    int y= GetMouseY();
-     if(x>180 && x<300 ){
-        if(y>100 && y<215){// First Cell
-        cout<<"First Cell"<<endl;
-        if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
-            if(board[0][0]==0){
-            playermove(1);}
-            
-        }
-    }
-        else if(y>216 && y<385){// Fourth Cell
-            cout<<"Fourth Cell"<<endl;
-         if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
-            if(board[1][0]==0)
-            playermove(4);
-            
-        }
-        }
-        else if(y>386 && y<500){// Seventh Cell
-            cout<<"Seventh Cell"<<endl;
-            if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
-                if(board[2][0]==0)
-            playermove(7);
-            
-        }
-        }
-    }
-     else if(x>301 && x<500 ){// second Cell
-        if( y>100 && y<215){
-        cout<<"Second Cell"<<endl;
-        if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
-            if(board[0][1]==0)
-            playermove(2);
-            
-        }
-    }
-        else if(y>216 && y<385){// Fifth Cell
-            cout<<"Fifth Cell"<<endl;
-            if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
-                if(board[1][1]==0)
-            playermove(5);
-        }
-        }
-        else if(y>386 && y<500){ // Eighth Cell
-            cout<<"Eight Cell"<<endl;
-            if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
-                if(board[2][1]==0)
-            playermove(8);
-            
-        }
-        }
-    }
-    else if(x>501 && x<620 ){// Third Cell
-        if( y>100 && y<215){
-        cout<<"Third Cell"<<endl;
-        if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
-            if(board[0][2]==0){
-           playermove(3);}
-            
-        }
-    }
-        else if(y>216 && y<385){
-            cout<<"Sixth Cell"<<endl;
-            if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
-                if(board[1][2]==0){
-            playermove(6);}
-            
-        }
-        }
-        else if(y>386 && y<500){
-            cout<<"Nineth Cell"<<endl;
-            
-            if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
-                if(board[2][2]==0){
-            playermove(9);}
-            
-        }
-        }
-    }
-}
+void drawUI(){
+    DrawText("Player: ",10,10,40,WHITE);
+    if(!player)
+        DrawText("o",170,-20,100,BLUE);
+    else
+        DrawText("x",170,-20,100,RED);
 
-void clear_board(){
+    if(winner==1)
+        DrawText(" Player o Wins!",200,520,60,YELLOW);
+    else if(winner==2)
+        DrawText("Player x Wins!",200,520,60,YELLOW);
+    else if(counter==9)
+        DrawText("DRAW!",310,520,60,YELLOW);
+    
+    
+
+    
+}
+void drawGame(){
     ClearBackground(BLACK);
+    drawboard();
+    drawmarks();
+    drawUI();
 }
 int main(){
-
 InitWindow(width,height, "Hello World!");
 SetTargetFPS(60);
 system("cls");
 
 while (!WindowShouldClose()){
     BeginDrawing();
-    
-    drawboard();
-    
+    drawGame();
     int x=GetMouseX();
     int y=GetMouseY();
     
-    if(x>180 && x<621 && y>100 && y<501){
         system("cls");
         cout<<"Inside Board"<<endl;
-        if(!winner){
-            getmouseposition();}
+        cout<<"Cell: ["<<(GetMouseX()-180)/140<<",";
+        cout<< (GetMouseY()-100)/133<<"]"<<endl;
+        if(winner==0){
+            HandleInput();}
         for(int i=0; i<3; i++){
         for(int j=0; j<3; j++){
             cout<<board[i][j];
+        
         }
         cout<<endl;
-    }
-    }   
+            
+        }
+        
     EndDrawing();
-}
+    }
 }
